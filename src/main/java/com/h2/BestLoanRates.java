2ebef6fecd018dd149b800bf3ee3f301b19c9ada
kjlupe <42919063+kjlupe@users.noreply.github.com>
2020-07-01 16:49:09 -0400

    Completed module

--- src/main/java/com/h2/BestLoanRates.java
+++ src/main/java/com/h2/BestLoanRates.java
@@ -1,6 +1,7 @@
 package com.h2;
 
 import java.util.Scanner;
+import java.util.Map;
 
 /**
  * BestLoanRates
@@ -8,11 +9,18 @@ import java.util.Scanner;
  */
 
 public class BestLoanRates {
+    public static final Map<Integer, Float> bestRates = Map.of(
+        1, 5.50f,
+        2, 3.45f,
+        3, 2.67f
+    );
 
-    public static void main( String[] args ) {
-        Scanner scanner = new Scanner(System.in);
+    public static void main( final String[] args) {
+        final Scanner scanner = new Scanner(System.in);
         System.out.println("Enter your name");
-        String name = scanner.nextLine();
+        final String name = scanner.nextLine();
         System.out.println("Hello " + name);
+
+        scanner.close();
     }
 }
